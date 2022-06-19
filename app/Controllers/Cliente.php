46afda2162afafacf27294baa99e0c7157030f05
StarLord <blackhunterdev01@gmail.com>
2022-06-19 00:36:20 -0300

    dev: adding controllers 'Cliente' adn 'Saldo'

---
+++ app/Controllers/Cliente.php
@@ -0,0 +1,15 @@
+<?php
+
+class Cliente {
+    public $nome;
+    public $fone;
+    public 
+
+    function set_nome($nome) {
+        $this->nome = $nome;
+    }
+
+    function get_nome() {
+
+    }
+}
