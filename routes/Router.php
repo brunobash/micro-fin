e3e5880c0ba9b7291f63d6a92927c0269bb402a6
StarLord <blackhunterdev01@gmail.com>
2022-06-19 22:00:29 -0300

    dev: mudanças de foco

--- routes/Router.php
+++ routes/Router.php
@@ -1,16 +1,5 @@
 <?php
 
-require_once __DIR__ . '/RouteSwitch.php';
-
-
-class Router extends RouteSwitch {
-    public function run(string $requestUri) {
-        $route = substr($requestUri, 1); // eu sei que ta quebrando o SRP, mas sou burro e não sei como fazer diferente
-
-        if($route === '') {
-            $this->home();
-        } else {
-            $this->$route();
-        }
-    }
-}   
+/***
+ * underconstrucition...
+*/
